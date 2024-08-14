a2461b45419d5318fea5b7426fa6f1c41dc789ab
ruan-cat <1219043956@qq.com>
2024-08-14 16:16:36 +0800

    ⚙️ config:

--- server/api/create-pets-table.ts
+++ server/api/create-pets-table.ts
@@ -1,3 +1,5 @@
+// https://vercel.com/docs/storage/vercel-postgres/quickstart#create-a-table-in-your-database
+
 import type { VercelRequest, VercelResponse } from "@vercel/node";
 import { sql } from "@vercel/postgres";
 
@@ -6,6 +8,7 @@ export default async function handler(
   response: VercelResponse
 ) {
   try {
+    // TODO: 总不可能在这里新建表吧。 看看后续的 ORM 怎么维护数据库表。
     const result =
       await sql`CREATE TABLE Pets ( Name varchar(255), Owner varchar(255) );`;
     return response.status(200).json({ result });
